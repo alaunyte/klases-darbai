fa1c7e16b59121d0d7960033ff27bfaa69693ecb
BriBri <bribri@Arminass-MacBook-Pro.local>
2020-02-05 09:32:21 +0200

    trecia uzduotis

--- 0205.php
+++ 0205.php
@@ -6,6 +6,25 @@ $a = 2;
 $b = 1;
 
 if ($a > $b) {
-	print "a daugiau uz b";
+	print "a daugiau uz b  <br><br>";
 }
 
+// 3. istaisykite klaidas, rezultate turi spausdinti zodi "Vokietija"
+
+$salis = "de";
+
+switch ($salis) {
+	case 'ca':
+		echo ("Kanada");
+		break;
+	case 'cr':
+		echo ("Kosta Rika");
+		break;
+	case 'de':
+		echo ("Vokietija  <br><br>");
+		break;
+	case 'uk':
+		echo ("Didzioji Britanija");
+		break;
+	default;
+}
